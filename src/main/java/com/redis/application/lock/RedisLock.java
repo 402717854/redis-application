02906152c1610ce533844058df74356c4cde3b4b
wangyongsheng <wangyongsheng@haier.com>
2021-02-26 17:41:26 +0800

    redis锁续航

--- src/main/java/com/redis/application/lock/RedisLock.java
+++ src/main/java/com/redis/application/lock/RedisLock.java
@@ -13,37 +13,39 @@ public class RedisLock {
 
     public void lock(String key, String clientId, long expire)  {
         boolean lock = RedisUtils.lock(key, clientId, expire);
-        if(lock){
-            threadState=Boolean.TRUE;
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    while (threadState) {
-                        Long expire1 = RedisUtils.getExpire(key, TimeUnit.MILLISECONDS);
-                        if (expire1 <=0) {
-                            log.info("异步暂停线程");
-                            threadState=Boolean.FALSE;
-                        }
-                        log.info("lock存在剩余时间:{}",expire1);
-                        if (expire1> 1000 && expire1< 2000) {
-                            log.info("异步线程续航lock");
-                            RedisUtils.expire(key, expire);
-                        }
+        if(!lock){
+            log.info("获取锁失败");
+            return;
+        }
+        threadState=Boolean.TRUE;
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                while (threadState) {
+                    Long expire1 = RedisUtils.getExpire(key, TimeUnit.MILLISECONDS);
+                    if (expire1 <=0) {
+                        log.info("异步暂停线程");
+                        threadState=Boolean.FALSE;
+                    }
+                    log.info("lock存在剩余时间:{}",expire1);
+                    if (expire1> 1000 && expire1< 2000) {
+                        log.info("异步线程续航lock");
+                        RedisUtils.expire(key, expire);
                     }
                 }
-            };
-            Thread thread = new Thread(runnable);
-            thread.start();
-            try {
-                log.info("业务处理-----");
-                Thread.sleep(expire+5000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }finally {
-                RedisUtils.safeUnLock(key,clientId);
-                threadState=Boolean.FALSE;
-                log.info("释放锁");
             }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+        try {
+            log.info("业务处理-----");
+            Thread.sleep(expire+5000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            RedisUtils.safeUnLock(key,clientId);
+            threadState=Boolean.FALSE;
+            log.info("释放锁");
         }
     }
 }
